48f2ca9534cf48bfc24ecdd875e1dc1efd1e5531
Andrew <atklenner@gmail.com>
2022-01-21 12:33:45 -0800

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const indexController = require("../controllers/indexController");
+
 router.get("/", (req, res, next) => {
   res.redirect("/blog");
 });
+
+router.get("/blog", indexController.getAllBlogs);
+
+router.post("/blog", indexController.postNewBlog);
+
+router.get("/blog/:id", indexController.getBlogPost);
+
+router.patch("/blog/:id", indexController.updateBlogPost);
+
+router.delete("/blog/:id", indexController.deleteBlogPost);
+
+router.get("/blog/:id/comments", indexController.getBlogPostComments);
+
+router.post("/blog/:id/comments", indexController.postNewBlogPostComment);
+
+router.get("/blog/:id/comments/:id", indexController.getComment);
+
+router.patch("/blog/:id/comments/:id", indexController.updateComment);
+
+router.delete("/blog/:id/comments/:id", indexController.deleteComment);
 
 module.exports = router;
